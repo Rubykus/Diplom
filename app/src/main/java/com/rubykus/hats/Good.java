f21b38b272237749f0b2d1bb9e3fdb17ff6f67a6
Rubykus <yegor.yerak@gmai.com>
2016-05-26 16:31:59 +0300

    full working all screen with delete

---
+++ app/src/main/java/com/rubykus/hats/Good.java
@@ -0,0 +1,244 @@
+package com.rubykus.hats;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class Good extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int CM_DELETE_ID = 1;
+    private static final int CM_UPDATE_ID = 2;
+    ListView lv;
+    DB db;
+    SimpleCursorAdapter scAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // open a connection to the db
+        db = new DB(this);
+        db.open();
+
+        // forming matching columns
+        String[] from = new String[] { DB.COLUMN_ID, DB.GOOD_NAME, DB.GOOD_ID_CAT, DB.GOOD_COLOR,
+        DB.GOOD_SEX, DB.GOOD_FIRM, DB.GOOD_QUANTITY, DB.GOOD_PRICE, DB.GOOD_IMAGE };
+        int[] to = new int[] { R.id.text1, R.id.text2, R.id.text3, R.id.text4, R.id.text5,
+                R.id.text6, R.id.text7, R.id.text8, R.id.text9};
+
+        // create adapter and customizable list
+        scAdapter = new SimpleCursorAdapter(this, R.layout.item_good, null, from, to, 0);
+        lv = (ListView) findViewById(R.id.lv);
+        lv.setAdapter(scAdapter);
+
+        // add context menu for list
+        registerForContextMenu(lv);
+
+        // create loader for reading data
+        getSupportLoaderManager().initLoader(0, null, this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.add) {
+            /*View view = LayoutInflater.from(this).inflate(R.layout.dialog, null);
+            final EditText nameCat = (EditText)view.findViewById(R.id.nameAddCat);
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.new_cat)
+                    .setView(view)
+                    .setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String textNameCat = nameCat.getText().toString();
+                            db.addCat(textNameCat);
+                            getSupportLoaderManager().getLoader(0).forceLoad();
+                        }
+                    })
+                    .setNegativeButton(R.string.cancle, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+            AlertDialog dialog = builder.create();
+            dialog.show();*/
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.categories) {
+            Intent intent = new Intent(this, Categories.class);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.goods) {
+            onBackPressed();
+        } else if (id == R.id.checks) {
+            Intent intent = new Intent(this, Check.class);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.sales) {
+            Intent intent = new Intent(this, Sale.class);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.about_us) {
+
+        } else if (id == R.id.exit) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    // my cod
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, CM_UPDATE_ID, 0, R.string.update);
+        menu.add(0, CM_DELETE_ID, 0, R.string.delete);
+    }
+
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == CM_DELETE_ID) {
+            // obtain from the context menu item list data
+            AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) item
+                    .getMenuInfo();
+            // retrieve the record id and delete the corresponding entry in the database
+            db.delGood(acmi.id);
+            // obtain new cursor with data
+            getSupportLoaderManager().getLoader(0).forceLoad();
+            return true;
+        } else {
+            /*Cursor m = scAdapter.getCursor();
+            View view = LayoutInflater.from(this).inflate(R.layout.dialog, null);
+            final EditText nameCat = (EditText)view.findViewById(R.id.nameAddCat);
+            final String newTextNameCat = m.getString(m.getColumnIndex(DB.CAT_NAME));
+            nameCat.setText(newTextNameCat);
+            final AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) item
+                    .getMenuInfo();
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.edit_cat)
+                    .setView(view)
+                    .setPositiveButton(R.string.update, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String textNameCat = nameCat.getText().toString();
+                            db.updateCat(acmi.id, textNameCat);
+                            getSupportLoaderManager().getLoader(0).forceLoad();
+                        }
+                    })
+                    .setNegativeButton(R.string.cancle, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+            AlertDialog dialog = builder.create();
+            dialog.show();*/
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        // close connection
+        db.close();
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bndl) {
+        return new MyCursorLoader(this, db);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        scAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
+
+    static class MyCursorLoader extends CursorLoader {
+
+        DB db;
+
+        public MyCursorLoader(Context context, DB db) {
+            super(context);
+            this.db = db;
+        }
+
+        @Override
+        public Cursor loadInBackground() {
+            Cursor cursor = db.getAllGood();
+            return cursor;
+        }
+
+    }
+}
